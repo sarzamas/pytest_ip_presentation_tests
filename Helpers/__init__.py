b03347306630e3b729587f50f5b6db60ad1bedd0
sarzamas <sarzamas@users.noreply.github.com>
2024-05-16 02:09:51 +0300

    Update __init__.py

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -1,9 +1,9 @@
-import os
+from os import mkdir, path
 from typing import Union
 
-LOCAL_DIR_PATH = os.path.split(os.path.dirname(__file__))[0]  # type: Union[str, bytes] # Рабочая директория проекта
-IO_PATH = os.path.join(LOCAL_DIR_PATH, 'share')  # Директория для получения и выкладки объектов
-ASSETS_PATH = os.path.join(LOCAL_DIR_PATH, 'assets')  # Директория с исходными данными
+LOCAL_DIR_PATH = path.split(path.dirname(__file__))[0]  # type: Union[str, bytes] # Рабочая директория проекта
+IO_PATH = path.join(LOCAL_DIR_PATH, 'share')  # Директория для получения и выкладки объектов
+ASSETS_PATH = path.join(LOCAL_DIR_PATH, 'assets')  # Директория с исходными данными
 
-if not os.path.exists(IO_PATH):
-    os.mkdir(IO_PATH)
+if not path.exists(IO_PATH):
+    mkdir(IO_PATH)
